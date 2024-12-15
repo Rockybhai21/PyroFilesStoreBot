49a44c3fbe1dc6363d2d6eb9d0f828268f55b0b2
Rockybhai21 <rockybhai2109rs@gmail.com>
2024-12-15 16:34:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,6 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
     name=Config.BOT_USERNAME       
-    in_memory=True,
     bot_token=Config.BOT_TOKEN,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH)
